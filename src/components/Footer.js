3a9ba4cb4f2f1c662129e50016383f3d3d900936
badMeme <noveladdress@gmail.com>
2022-10-29 15:19:15 -0500

    css fixes. adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Containter } from 'react-bootstrap';
+// import { MailchimpForm } from './MailchimpForm';
+// import logo from ' ../assets/img/logo.svg';
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <Container>
+                <Row className='align-item-center'>
+                    <img src='' alt='Logo' />
+                    <Col>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
