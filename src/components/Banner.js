9bd9cba8e11f70ae2b678addcc363920ef13eee4
badMeme <noveladdress@gmail.com>
2022-10-31 11:06:28 -0500

    still resolving errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
-import {ArrowRightCircle} from 'react-bootstrap-icons'
+// import {ArrowRightCircle} from 'react-bootstrap-icons'
 import headerImg from "../assets/img/NicePng_vaporwave-grid-png_2071258.png" ///stuff///
 
 const Banner = () => {
@@ -8,17 +8,11 @@ const Banner = () => {
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random()*100)
+    // const [index, setIndex] = useState(1);
     const toRotate = ["Software Developer", "Web Developer", "Web Designer", "UI/UX Designer", "Change These Banner.js"];
     const period = 2000;
 
-    useEffect(() => {
-        let counter = setInterval(() => {
-            count();
-        }, delta)
-
-        return () => {clearInterval(counter)};
-    }, [text])
-
+    
     const count = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
@@ -33,15 +27,25 @@ const Banner = () => {
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
             setDelta(period);
-        } else if(isDeleting && updatedText === '') {
+        } else if (isDeleting && updatedText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
             setDelta(500)
-        }
+        } 
     
     }
 
+    useEffect(() => {
+        let counter = setInterval(() => {
+            count();
+        }, delta);
+
+        return () => {clearInterval(counter)};
+    })
+
+
     const bio="A software engineer with passions for art, music, poetry, philosophy, and programming. I'm an analytical and creative mind that is always seeking to broaden my horizons and solve interesting problems. I’m looking for a backend development role where I can continue to expand my knowledge and skills."
+    
     return (
         <section className="banner" id="home">
             <Container>
@@ -49,7 +53,7 @@ const Banner = () => {
                     <Col xs={12} md={6} xl={7}>
                         <span className='tagline'>Corby's Portfolio</span>
                         <h1>{"Corey Neal"}</h1>
-                        <h2 className="txt-rotate" data-period="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></h2>
+                        <h2 className="txt-rotate" data-period="1000" ><span className="wrap">{text}</span></h2>
                         <p>{bio}</p>
                         {/* <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button> */}
                     </Col>
